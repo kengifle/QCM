63760d39f30c90a0ad16ee847c5961149ff3320a
kengifle <43824033+kengifle@users.noreply.github.com>
2018-10-07 15:50:53 +0200

    Add files via upload
    sauvegarde dimanche 0710
    insert question + 4 reponses
    affichage toutes les questions
    affichage toutes les reponses

---
+++ accueil_prof.php
@@ -0,0 +1,24 @@
+<?php session_start()?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>ACCUEIL : Professeurs</title>
+	</head>
+	<body>
+		<!--utilisation de la variable de session nom_user-->
+		<p>
+		<a><?php echo 'Bienvenue, M ou Mme  '.$_SESSION['nom_user'].' , sur ce playground de QCM.';?></a></p>
+
+		<p>
+		<a href="question_ajouter.php">Voulez vous saisir une question?</a>
+
+		<p>
+
+		<p><a href="question_afficher.php">Voulez-vous afficher les questions?</a></p>
+
+		<p><a href="reponse_afficher.php">Voulez-vous afficher les réponses?</a></p>
+		<!--lien vers page deconnexion qui detruira la session et renverra à l'index-->
+		<a href="deconnexion.php"> se deconnecter </a></p>
+	</body>
+</html>
