e8f95f3b6629fce94daccebdffd338c8113551fe
kengifle <43824033+kengifle@users.noreply.github.com>
2018-11-15 19:58:41 +0100

    Add files via upload
    dernières fonctionnalités en cours
    BDD MODIFIEE AJOUT CHAMP LABEL_QCM dans table QCM

---
+++ qcm_repondre_qcm.php
@@ -0,0 +1,23 @@
+<?php session_start()?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>affichage questions</title>
+	</head>
+	<body>
+		<?php include('connexion.php');?>
+		<!--formulaire-->
+		<form action="traitement_qcm_repondre_qcm.php" method="POST">
+			<!--affichage de la liste des questions dans un select-->
+			<?php
+			include('connexion.php');
+			$reponse = $linkpdo->query("SELECT label_qcm, id_qcm_fk, id_user_fk FROM qcm");?>
+			<select title ="choississez dans la liste" name="liste_de_qcm"><?php
+				foreach ($reponse as $data)
+				{echo '<option value="' . $data['id_qcm_fk'] . '">' . $data['label_qcm'] . '</option>';}
+			?></select>
+			<input type="submit" value= "repondre au qcm?" name="repondre_qcm">
+		</form>
+		</body>
+	</html>
